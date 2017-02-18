605d225a675015d5dcdab9e18b1c9286c76ae7d6
lsenft <lsenft@gmail.com>
2017-02-17 20:51:36 -0700

    Add use statements.

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,5 +1,6 @@
 <?php
+use Cake\Core\Configure;
+use Lsenft\CakephpExceptionNotifier\Error\ExceptionNotifierErrorHandler;
 
-//use Lsenft\CakephpExceptionNotifier\Error\ExceptionNotifierErrorHandler;
 $errorHandler = new ExceptionNotifierErrorHandler();
 $errorHandler->register();
